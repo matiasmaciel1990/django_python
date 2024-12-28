77beeaabe76acbbaf29f90356e9d69416f4b6f4b
bitaliano <94454334+bitaliano@users.noreply.github.com>
2024-12-27 22:16:57 -0300

    add producto

--- productos/models.py
+++ productos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Producto(models.Model):
+    codigo = models.CharField(max_length=50, unique=True)
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    fabricante = models.CharField(max_length=100)
+    precio = models.FloatField()
+
+    def __str__(self):
+        return self.nombre
