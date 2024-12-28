a6321ee46e61ddb86efe549a804f28c6b70bf702
bitaliano <94454334+bitaliano@users.noreply.github.com>
2024-12-28 20:21:50 -0300

    add vista s

--- clientes/views.py
+++ clientes/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def vista_inicio(request):
+    return HttpResponse("¡Bienvenido a la gestión de clientes!")
