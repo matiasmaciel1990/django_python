063aa6675da06f5d3a3c1a443d6cbf2dba1c400e
bitaliano <94454334+bitaliano@users.noreply.github.com>
2024-12-27 21:52:38 -0300

    add pedidos

--- pedidos/models.py
+++ pedidos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Pedido(models.Model):
+    numero = models.AutoField(primary_key=True)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    productos = models.ManyToManyField(Producto, through='DetallePedido')
+
+    def __str__(self):
+        return f"Pedido #{self.numero} - Cliente: {self.cliente.nombre}"
